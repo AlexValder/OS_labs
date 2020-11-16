40ff51de71c5d1416259672b826fd9671558ce85
AlexValder <anna.loban@yahoo.com>
2020-11-17 00:37:14 +0200

    Lab03 preparation

---
+++ лаба3/PortableApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PortableConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"Hi! This is a console app." +
+                $"\n\n" +
+                $"Username: {Environment.UserName}\n" +
+                $"Machine: {Environment.MachineName}\n" +
+                $"OS: {Environment.OSVersion}\n" +
+                $"Is x64 OS? {Environment.Is64BitOperatingSystem}\n" +
+                $"Is x64 process? {Environment.Is64BitProcess}\n" +
+                $"Processor count: {Environment.ProcessorCount}\n" +
+                $"\n\n" +
+                $"Press any button to exit.\n");
+            Console.ReadKey();
+        }
+    }
+}
